efad179e53bd8a2737faa7a0da639e9a3044b6b1
橙续缘 <lixiaohui2019@sina.cn>
2020-04-24 17:59:41 +0800

    feat: 线上环境配置文件

---
+++ config/config.prod.js
@@ -0,0 +1,9 @@
+'use strict';
+/** @param {Egg.EggAppInfo} appInfo */
+module.exports = appInfo => {
+  /** @type {Egg.EggAppConfig} */
+  const config = exports = {};
+
+  siteDoname = 'https://xiaolandian.leexiaohui.top/';
+  return config;
+};
