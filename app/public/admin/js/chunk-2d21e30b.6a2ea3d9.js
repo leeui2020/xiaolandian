7bdd8164609ad754329be6d85b4dd8a69e182ab5
leeui2020 <1287338537@qq.com>
2020-04-25 11:59:22 +0800

    release: admin构建

---
+++ app/public/admin/js/chunk-2d21e30b.6a2ea3d9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21e30b"],{d504:function(e,n,a){"use strict";a.r(n);var t=function(){var e=this,n=e.$createElement,a=e._self._c||n;return a("div",[e._v("首页")])},c=[],u={name:"Index"},r=u,s=a("2877"),d=Object(s["a"])(r,t,c,!1,null,"0446acaa",null);n["default"]=d.exports}}]);
+//# sourceMappingURL=chunk-2d21e30b.6a2ea3d9.js.map
